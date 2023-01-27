91fadc205cb54a6cddbb302f96493c7d5fd89aea
Rick Phillips <rickwphillips@gmail.com>
2023-01-27 16:27:44 -0500

    Layout and Styling + Added card, URL field, & File upload field to index.hbs + Updated title and added description to index.js + Added dev/nodemon, formidable, and updated versions in package.json

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,12 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const file = require('');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', {
+    title: 'Node.js Schema Validator',
+    description: 'Custom JSON schema validator build on Node.js, Express, and Bootstrap' });
 });
 
 module.exports = router;
